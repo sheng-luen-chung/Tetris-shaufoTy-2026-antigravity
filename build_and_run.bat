8e5545de9e35b123f99c819eb9686f243b8c13ef
sheng-luen-chung <sheng.luen.chung@gmail.com>
2026-06-02 17:12:28 +0800

    Configure Windows JDK 25 repo runner

---
+++ build_and_run.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal
+
+set "PROJECT_ROOT=%~dp0"
+cd /d "%PROJECT_ROOT%"
+
+set "JDK25_HOME=C:\Program Files\Eclipse Adoptium\jdk-25.0.2.10-hotspot"
+if exist "%JDK25_HOME%\bin\javac.exe" (
+    set "JAVA_HOME=%JDK25_HOME%"
+    set "PATH=%JAVA_HOME%\bin;%PATH%"
+)
+
+echo Using java from:
+where java
+echo Using javac from:
+where javac
+echo.
+java -version
+javac -version
+echo.
+
+if not exist "bin" mkdir "bin"
+
+javac -d bin src\com\tetris\main\Main.java src\com\tetris\controller\*.java src\com\tetris\model\*.java src\com\tetris\view\*.java src\com\tetris\util\*.java src\com\tetris\test\*.java
+if errorlevel 1 exit /b %errorlevel%
+
+java -cp bin com.tetris.main.Main
